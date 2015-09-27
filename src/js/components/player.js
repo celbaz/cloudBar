36c1414356449b8e0ab162960de84731e2928d5d
Carl Elbaz <carl.elbaz@gmail.com>
2015-09-27 17:03:56 -0400

    updated styles

--- src/js/components/player.js
+++ src/js/components/player.js
@@ -12,7 +12,92 @@ var AudioPlayer = React.createClass({
     return { playing: false};
   },
 
+  playPause: function (event) {
+    $(event.target).toggleClass('fa-pause').toggleClass('fa-play')
+    event.preventDefault();
+    window.animiateGooey();
+  },
+
   componentDidMount: function () {
+    var $cover=$(".player-cover")
+  		,$bars=$(".player-spectrum-bar")
+  		,$spectrum=$(".player-spectrum")
+
+  		,paused=true
+
+  		,barWidth=$bars.width()
+  		,barHeight=$bars.height()
+
+  		,coverPos=$cover.position()
+  		,coverSize=$cover.width()
+  		,coverCenter=coverPos.left+(coverSize/2)
+  		,coverRadius=coverSize/2
+
+  		,spectrumPos=$spectrum.position().top
+  		,spectrumMargin=parseFloat($spectrum.css("marginTop"))
+  	;
+
+  	$.fn.animBar=function(v){
+  		var $bar=$(this);
+
+  		var min=0.5;
+  		var setV=true
+  		if(typeof v=="undefined"){
+  			v=min+(Math.random()*(1-min));
+  			setV=false;
+  		}else{
+  			// v=Math.max(min,v)
+  		}
+
+  		if(v>parseFloat($bar.attr("data-scale")) && v>min){
+  			TweenMax.to($bar,v*(setV?0.6:0.2),{
+  				scaleY:v,
+  				attr:{
+  					"data-scale":v
+  				},
+  				ease:Quad.easeOut,
+  				onComplete:function(){
+  					TweenMax.to($bar,v*0.6,{
+  						scaleY:0.1,
+  						attr:{
+  							"data-scale":0.1
+  						},
+  						ease:Quad.easeIn,
+  						onComplete:function(){
+  							$bar.animBar(v*0.5)
+  						}
+  					});
+  				},
+  				onUpdate:function(){
+  				}
+  			});
+
+  		}
+  		if(!setV && !paused){
+  			setTimeout(function(){
+  				$bar.animBar()
+  			},Math.random()*500);
+  		}
+  	}
+  	function start() {
+  		$bars.each(function(){
+  			var $cur=$(this);
+  			$cur.attr("data-scale",0.5);
+  			TweenMax.set($cur,{
+  				scaleY:0
+  			});
+  			$cur.animBar();
+  		})
+  	}
+
+  	start();
+
+  	window.animiateGooey = function() {
+  		paused=!paused;
+  		if(!paused) {
+  			start();
+  		}
+  	};
   },
 
   render: function () {
@@ -26,11 +111,13 @@ var AudioPlayer = React.createClass({
 							<h3 className="player-artist">Glass Animals</h3>
 						</div>
 						<div className="player-cover">
+              <img src="assets/default.jpg" width="300"/>
 						</div>
 					</div>
 					<div className="player-spectrum">
 						<div className="player-spectrum-bars">
 							<div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
 							<div className="player-spectrum-bar"></div>
 							<div className="player-spectrum-bar"></div>
 							<div className="player-spectrum-bar"></div>
@@ -48,7 +135,6 @@ var AudioPlayer = React.createClass({
 							<div className="player-spectrum-bar"></div>
 							<div className="player-spectrum-bar"></div>
 						</div>
-						<div className="player-spectrum-floor"></div>
 					</div>
 					<div className="player-controls">
 						<div className="player-buttons">
@@ -58,7 +144,7 @@ var AudioPlayer = React.createClass({
 							<button className="player-button">
 								<i className="fa fa-step-backward"></i>
 							</button>
-							<button className="player-button play-pause-button">
+							<button className="player-button play-pause-button" onClick= {this.playPause}>
 								<i className="fa fa-pause"></i>
 							</button>
 							<button className="player-button">
@@ -73,7 +159,4 @@ var AudioPlayer = React.createClass({
     );
   }
 });
-
-
 module.exports = AudioPlayer;
-// html
