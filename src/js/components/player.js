bad61c19171a9d45e825df54e70d32795128d831
Carl Elbaz <carl.elbaz@gmail.com>
2015-09-27 18:35:54 -0400

    Uniform spacing in player.js and add .main-container to container element.

--- src/js/components/player.js
+++ src/js/components/player.js
@@ -29,141 +29,141 @@ var AudioPlayer = React.createClass({
 
   componentDidMount: function () {
     var $cover=$(".player-cover")
-  		,$bars=$(".player-spectrum-bar")
-  		,$spectrum=$(".player-spectrum")
+      ,$bars=$(".player-spectrum-bar")
+      ,$spectrum=$(".player-spectrum")
 
-  		,paused= (!AudioStore.currentlyPlaying())
+      ,paused= (!AudioStore.currentlyPlaying())
 
-  		,barWidth=$bars.width()
-  		,barHeight=$bars.height()
+      ,barWidth=$bars.width()
+      ,barHeight=$bars.height()
 
-  		,coverPos=$cover.position()
-  		,coverSize=$cover.width()
-  		,coverCenter=coverPos.left+(coverSize/2)
-  		,coverRadius=coverSize/2
+      ,coverPos=$cover.position()
+      ,coverSize=$cover.width()
+      ,coverCenter=coverPos.left+(coverSize/2)
+      ,coverRadius=coverSize/2
 
-  		,spectrumPos=$spectrum.position().top
-  		,spectrumMargin=parseFloat($spectrum.css("marginTop"))
-  	;
+      ,spectrumPos=$spectrum.position().top
+      ,spectrumMargin=parseFloat($spectrum.css("marginTop"))
+    ;
 
-  	$.fn.animBar=function(v){
-  		var $bar=$(this);
+    $.fn.animBar=function(v){
+      var $bar=$(this);
 
-  		var min=0.5;
-  		var setV=true
-  		if(typeof v=="undefined"){
-  			v=min+(Math.random()*(1-min));
-  			setV=false;
-  		}else{
-  			// v=Math.max(min,v)
-  		}
+      var min=0.5;
+      var setV=true
+      if(typeof v=="undefined"){
+        v=min+(Math.random()*(1-min));
+        setV=false;
+      }else{
+        // v=Math.max(min,v)
+      }
 
-  		if(v>parseFloat($bar.attr("data-scale")) && v>min){
-  			TweenMax.to($bar,v*(setV?0.6:0.2),{
-  				scaleY:v,
-  				attr:{
-  					"data-scale":v
-  				},
-  				ease:Quad.easeOut,
-  				onComplete:function(){
-  					TweenMax.to($bar,v*0.6,{
-  						scaleY:0.1,
-  						attr:{
-  							"data-scale":0.1
-  						},
-  						ease:Quad.easeIn,
-  						onComplete:function(){
-  							$bar.animBar(v*0.5)
-  						}
-  					});
-  				},
-  				onUpdate:function(){
-  				}
-  			});
+      if(v>parseFloat($bar.attr("data-scale")) && v>min){
+        TweenMax.to($bar,v*(setV?0.6:0.2),{
+          scaleY:v,
+          attr:{
+            "data-scale":v
+          },
+          ease:Quad.easeOut,
+          onComplete:function(){
+            TweenMax.to($bar,v*0.6,{
+              scaleY:0.1,
+              attr:{
+                "data-scale":0.1
+              },
+              ease:Quad.easeIn,
+              onComplete:function(){
+                $bar.animBar(v*0.5)
+              }
+            });
+          },
+          onUpdate:function(){
+          }
+        });
 
-  		}
-  		if(!setV && !paused){
-  			setTimeout(function(){
-  				$bar.animBar()
-  			},Math.random()*500);
-  		}
-  	}
-  	function start() {
-  		$bars.each(function(){
-  			var $cur=$(this);
-  			$cur.attr("data-scale",0.5);
-  			TweenMax.set($cur,{
-  				scaleY:0
-  			});
-  			$cur.animBar();
-  		})
-  	}
+      }
+      if(!setV && !paused){
+        setTimeout(function(){
+          $bar.animBar()
+        },Math.random()*500);
+      }
+    }
+    function start() {
+      $bars.each(function(){
+        var $cur=$(this);
+        $cur.attr("data-scale",0.5);
+        TweenMax.set($cur,{
+          scaleY:0
+        });
+        $cur.animBar();
+      })
+    }
 
-  	start();
+    start();
 
-  	window.animiateGooey = function() {
-  		paused=!paused;
-  		if(!paused) {
-  			start();
-  		}
-  	};
+    window.animiateGooey = function() {
+      paused=!paused;
+      if(!paused) {
+        start();
+      }
+    };
   },
 
   render: function () {
     return (
-      <div className="">
+      <div className="main-container">
         <div className="content">
-				<div className="player">
-					<div className="player-info">
-						<div className="player-info-text">
-							<h2 className="player-song-name">Gooey</h2>
-							<h3 className="player-artist">Glass Animals</h3>
-						</div>
-						<div className="player-cover">
+        <div className="player">
+          <div className="player-info">
+            <div className="player-info-text">
+              <h2 className="player-song-name">Gooey</h2>
+              <h3 className="player-artist">Glass Animals</h3>
+            </div>
+            <div className="player-cover">
               <img src="assets/default.jpg" width="300"/>
-						</div>
-					</div>
-					<div className="player-spectrum">
-						<div className="player-spectrum-bars">
-							<div className="player-spectrum-bar"></div>
+            </div>
+          </div>
+          <div className="player-spectrum">
+            <div className="player-spectrum-bars">
               <div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-							<div className="player-spectrum-bar"></div>
-						</div>
-					</div>
-					<div className="player-controls">
-						<div className="player-buttons">
-							<button className="player-button">
-								<i className="fa fa-bars"></i>
-							</button>
-							<button className="player-button">
-								<i className="fa fa-step-backward"></i>
-							</button>
-							<button className="player-button play-pause-button" onClick= {this.playPause}>
-								<i className="fa fa-pause"></i>
-							</button>
-							<button className="player-button">
-								<i className="fa fa-step-forward"></i>
-							</button>
-						</div>
-						<div className="player-slider"></div>
-					</div>
-				</div>
-			</div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+              <div className="player-spectrum-bar"></div>
+            </div>
+          </div>
+          <div className="player-controls">
+            <div className="player-buttons">
+              <button className="player-button">
+                <i className="fa fa-bars"></i>
+              </button>
+              <button className="player-button">
+                <i className="fa fa-step-backward"></i>
+              </button>
+              <button className="player-button play-pause-button" onClick= {this.playPause}>
+                <i className="fa fa-pause"></i>
+              </button>
+              <button className="player-button">
+                <i className="fa fa-step-forward"></i>
+              </button>
+            </div>
+            <div className="player-slider"></div>
+          </div>
+        </div>
+      </div>
     </div>
     );
   }
